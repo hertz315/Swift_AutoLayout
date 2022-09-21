24e84a1dc56607d7fad8181e29f4b3c30165e231
hertz315 <hertz315@gmail.com>
2022-09-21 16:03:52 +0900

    오토레이아웃

---
+++ Dailly_Challenge_reviewList/Dailly_Challenge_reviewList/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dailly_Challenge_reviewList
+//
+//  Created by Hertz on 9/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
