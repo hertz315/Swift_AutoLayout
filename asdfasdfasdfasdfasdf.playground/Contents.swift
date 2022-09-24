95f7b9ab5fa5b08a3d3136121001d549dedfb322
hertz315 <hertz315@gmail.com>
2022-09-24 14:12:12 +0900

    update

--- asdfasdfasdfasdfasdf.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-var greeting = "Hello, playground"
-
-let now = Date()
-var datefommater = DateFormatter()
-datefommater.dateFormat = "YYYY.MM.dd"
-print(datefommater)
-datefommater.string(from: now)
